5f3a50e48f01c374fff5f97dff094eca06461e64
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-05-12 18:22:10 -0300

    get notifications

--- app/api/webhooks/mercadopago/notifications/route.ts
+++ app/api/webhooks/mercadopago/notifications/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
     const status = request.nextUrl.searchParams.get('status') as string;
-    console.log(status)
+    console.log("[STATUS]: ", status)
+    console.log("[REQUEST]: ", request)
     return NextResponse.json({ success: true });
 }
